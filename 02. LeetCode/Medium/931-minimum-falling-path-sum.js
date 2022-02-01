8c18e467cee0e45fb1007dfffb3d840f5976cd6c
Sagar Kharabe <45946785+sagarkharabe@users.noreply.github.com>
2022-02-01 11:30:42 +0530

    Update 931-minimum-falling-path-sum.js

--- 02. LeetCode/Medium/931-minimum-falling-path-sum.js
+++ 02. LeetCode/Medium/931-minimum-falling-path-sum.js
@@ -2,6 +2,8 @@
  * @param {number[][]} matrix
  * @return {number}
  */
+
+/** Top Down */ 
 var minFallingPathSum = function(matrix) {
     const n = matrix.length;
     const dp = new Array(n).fill(-1).map(() => new Array(n).fill(Infinity));
@@ -23,4 +25,34 @@ var minFallingPathSum = function(matrix) {
     }
     
     return Math.min(...dp[n - 1])
+};
+
+/** Bottom Up*/
+var minFallingPathSum = function(matrix) {
+    const n = matrix.length;
+    const memo = new Array(n).fill(-1).map(() => new Array(n).fill(-1));
+    let min = Infinity;
+    
+    const dp = (i = n - 1, j = n - 1) => {
+
+        if(i === 0) return matrix[i][j];
+        
+        if(memo[i][j] === -1) {
+            const up = matrix[i][j] + dp(i - 1, j);
+            
+            const leftDiag = (j - 1) >= 0 ? matrix[i][j] + dp(i - 1,j - 1) : 10000;
+            
+            const rightDiag = (j + 1) < n ? matrix[i][j] + dp(i - 1,j + 1) : 10000;
+            
+            memo[i][j] = Math.min(up, leftDiag, rightDiag);
+        }
+        
+        return memo[i][j];
+    }
+    
+    for(let j = 0 ; j < n; j++) {
+        min = Math.min(min, dp(n - 1, j));  
+    }
+    
+    return min;
 };
