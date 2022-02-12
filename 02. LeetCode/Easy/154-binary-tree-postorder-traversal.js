fd39944eed4a6804b1ff38f5d2d899191f9fda5b
Sagar Kharabe <45946785+sagarkharabe@users.noreply.github.com>
2022-02-12 10:42:28 +0530

    Create 154-binary-tree-postorder-traversal.js

---
+++ 02. LeetCode/Easy/154-binary-tree-postorder-traversal.js
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var postorderTraversal = function(root) {
+    if(!root) return [];
+    
+    let result = [];
+    if(root.left) result = result.concat(postorderTraversal(root.left));
+    if(root.right) result = result.concat(postorderTraversal(root.right));
+    result.push(root.val);
+    
+    return result;
+};
+
+var postorderTraversal = function(root) {
+    if(!root) return [];
+    
+    let result = [];
+    let stack = [root];
+    
+    while(stack.length) {
+        const current = stack.pop();
+        
+        if(current.left) stack.push(current.left);
+        if(current.right) stack.push(current.right);
+             
+        result.unshift(current.val)
+    }
+    
+    return result;
+}
+
